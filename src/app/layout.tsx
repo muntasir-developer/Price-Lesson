2743de1de2d04a3cc95cda4b914a64d0ef803d7f
Md Muntasir <mdmuntasirbhai@gmail.com>
2025-10-15 14:48:17 +0530

    Add some chages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import NavbarWrapper from "@/components/NavbarWrapper";
 import { Toaster } from "react-hot-toast";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,6 +34,7 @@ export default function RootLayout({
       >
         <NavbarWrapper />
         {children}
+        <Analytics />
         <Toaster
           position="top-right"
           toastOptions={{
