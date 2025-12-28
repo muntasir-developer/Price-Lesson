cf1c11f6327d02d36c181889c96c4ef43ffe562c
Md Muntasir <mdmuntasirbhai@gmail.com>
2025-12-28 22:31:46 +0530

    Add GTM

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -75,6 +75,20 @@ export default function RootLayout({
             src="https://www.facebook.com/tr?id=1371555451046475&ev=PageView&noscript=1"
           />
         </noscript>
+
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-W77DQ6DR');
+            `,
+          }}
+        />
       </head>
 
       <body
@@ -83,6 +97,14 @@ export default function RootLayout({
         <Fpixel />
         <GoogleAnalytics />
         <NavbarWrapper />
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-W77DQ6DR"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         {children}
         <Analytics />
         <Toaster
